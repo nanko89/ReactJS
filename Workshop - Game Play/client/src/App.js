d0d21a84889538818ca59a71bd2f517b55ea11db
Natali Angelova <natali.v.angelova@gmail.com>
2023-02-28 12:57:12 +0200

    Add React Router

--- Workshop - Game Play/client/src/App.js
+++ Workshop - Game Play/client/src/App.js
@@ -1,6 +1,13 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home/Home.js";
 import { Navigation } from "./components/Navigation/Navigation.js";
+import { Login } from "./components/Login/Login.js";
+import { Register } from "./components/Register/Register.js";
+import { Catalog } from "./components/Catalog/Catalog.js";
+import { Details } from "./components/Details/Details.js";
+import { Edit } from "./components/Edit/Edit.js";
+import { Create } from "./components/Create/Create.js";
 
 function App() {
     return (
@@ -8,7 +15,15 @@ function App() {
             <Navigation />
             {/* Main Content */}
             <main id="main-content" />
-            <Home />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/catalog" element={<Catalog />} />
+                <Route path="/catalog/:id" element={<Details />} />
+                <Route path="/create" element={<Create />} />
+                <Route path="/catalog/edit/:id" element={<Edit />} />
+            </Routes>
         </div>
     );
 }
